d82f0425954694f509d02ce1359f5b027c60dbbd
jakubhorinek <jakubh@salsitasoft.com>
2022-01-17 11:06:25 +0100

    Update main.js
    process env fun test 2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+process.env.FUN = 'FUN';
 
 app.get('/', (req, res) => {
   res.send(`Hello Deployer, having ${process.env.FUN}!`)
